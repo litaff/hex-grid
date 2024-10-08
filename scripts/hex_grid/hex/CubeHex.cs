0edbb4ea096677c13cf46bbedce61c8a79c4cf18
litaff <a.gazda2k18@gmail.com>
2024-10-09 01:02:03 +0200

    Added mesh and size data to hex

--- scripts/hex_grid/hex/CubeHex.cs
+++ scripts/hex_grid/hex/CubeHex.cs
@@ -5,14 +5,20 @@ using vector;
 public class CubeHex
 {
     public CubeHexVector Position { get; private set; }
+    public float Size { get; private set; }
+    public int LibraryIndex { get; private set; }
 
-    public CubeHex()
+    public CubeHex(float size, int libraryIndex)
     {
         Position = CubeHexVector.Zero;
+        LibraryIndex = libraryIndex;
+        Size = size;
     }
     
-    public CubeHex(int q, int r)
+    public CubeHex(int q, int r, float size, int libraryIndex)
     {
         Position = new CubeHexVector(q, r);
+        LibraryIndex = libraryIndex;
+        Size = size;
     }
 }
