89c5fe3e1f7543e1d5c5253ac7aa2279e2f0dde1
litaff <a.gazda2k18@gmail.com>
2024-10-09 21:16:16 +0200

    Simplified primitive mesh generator

--- addons/hex_grid_editor/PrimitiveHexGridMesh.cs
+++ addons/hex_grid_editor/PrimitiveHexGridMesh.cs
@@ -10,7 +10,6 @@ using scripts.hex_grid;
 
 public class PrimitiveHexGridMesh
 {
-    private readonly World3D scenario;
     private readonly float cellSize;
     private readonly Material defaultMaterial;
     private Rid meshRid;
@@ -18,27 +17,17 @@ public class PrimitiveHexGridMesh
 
     public PrimitiveHexGridMesh(World3D scenario, float cellSize, Material defaultMaterial)
     {
-        this.scenario = scenario;
         this.cellSize = cellSize;
         this.defaultMaterial = defaultMaterial;
+        instanceRid = RenderingServer.InstanceCreate();
+        meshRid = RenderingServer.MeshCreate();
+        RenderingServer.InstanceSetBase(instanceRid, meshRid);
+        RenderingServer.InstanceSetScenario(instanceRid, scenario.Scenario);
+        RenderingServer.InstanceSetTransform(instanceRid, new Transform3D(Basis.Identity, Vector3.Zero));
     }
     
     public void UpdateMesh(CubeHex[] hexes)
     {
-        if (!meshRid.IsValid)
-        {
-            meshRid = RenderingServer.MeshCreate();
-        }
-        if (!instanceRid.IsValid)
-        {
-            instanceRid = RenderingServer.InstanceCreate();
-        }
-        if (instanceRid.IsValid && meshRid.IsValid)
-        {
-            RenderingServer.InstanceSetBase(instanceRid, meshRid);
-            RenderingServer.InstanceSetScenario(instanceRid, scenario.Scenario);
-            RenderingServer.InstanceSetTransform(instanceRid, new Transform3D(Basis.Identity, Vector3.Zero));
-        }
         var meshData = new Array();
         meshData.Resize((int)RenderingServer.ArrayType.Max);
         var meshVertices = new List<Vector3[]>();
@@ -54,10 +43,7 @@ public class PrimitiveHexGridMesh
         
         RenderingServer.MeshClear(meshRid);
         
-        if (meshVertices.Count == 0)
-        {
-            return;
-        }
+        if (meshVertices.Count == 0) return;
         
         RenderingServer.MeshAddSurfaceFromArrays(meshRid, RenderingServer.PrimitiveType.Triangles, meshData);
         RenderingServer.MeshSurfaceSetMaterial(meshRid, 0, defaultMaterial.GetRid());
