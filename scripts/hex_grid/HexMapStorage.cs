13c6aca26cba92ac8941420b9c201cd756973146
litaff <a.gazda2k18@gmail.com>
2024-10-10 20:07:50 +0200

    Implemented new overloads

--- scripts/hex_grid/HexMapStorage.cs
+++ scripts/hex_grid/HexMapStorage.cs
@@ -37,25 +37,17 @@ public class HexMapStorage
     
     public void Remove(CubeHexVector position)
     {
-        var hash = HashCode.Combine(position.Q, position.R);
-        map.Remove(hash);
+        map.Remove(position.GetHashCode());
     }
     
     public void Add(CubeHex value)
     {
-        var hash = HashCode.Combine(value.Position.Q, value.Position.R);
-        map.TryAdd(hash, value);
-    }
-
-    public CubeHex Get(int q, int r)
-    {
-        var hash = HashCode.Combine(q, r);
-        return map.GetValueOrDefault(hash);
+        map.TryAdd(value.Position.GetHashCode(), value);
     }
 
     public CubeHex Get(CubeHexVector hexPosition)
     {
-        return Get(hexPosition.Q, hexPosition.R);
+        return map.GetValueOrDefault(hexPosition.GetHashCode());
     }
     
     public CubeHex[] GetMap()
